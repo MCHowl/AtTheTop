293e24b0b7c930c8ea52432a150d43a53baf4b79
Matthew Chan <_mattc@hotmail.com>
2018-10-02 13:04:11 +0800

    Import Base Project

---
+++ AtTheTop/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+
+    [SerializeField]
+    GameObject player;
+
+    GameObject playerInstance;
+
+    Vector3 offset;
+
+    void Start() {
+        playerInstance = Instantiate(player);
+        offset = transform.position - player.transform.position;
+    }
+
+    void LateUpdate () {
+        transform.position = playerInstance.transform.position + offset;
+	}
+}
