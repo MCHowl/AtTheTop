8cfa513ddfb66a7a181ca49c56a7dac74764a5e1
Matthew Chan <_mattc@hotmail.com>
2018-10-23 21:07:17 +0800

    Added Calendar to the starting room

---
+++ AtTheTop/Assets/Scripts/Calendar.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Calendar : MonoBehaviour {
+
+    private TMPro.TextMeshPro calendarText;
+
+	void Start () {
+        calendarText = GetComponent<TMPro.TextMeshPro>();
+	}
+
+    private void Update() {
+        calendarText.text = GameData.CurrentDay.ToString() + "\nFeb";
+    }
+}
